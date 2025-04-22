2882bb1acdd6f0c49982f284f2bef3975f17b1f3
Lero4kaBu <124177009+Lero4kaBu@users.noreply.github.com>
2025-04-22 15:11:44 +0200

    Update task_1.m

--- task_1.m
+++ task_1.m
@@ -3,7 +3,7 @@ close all;
 
 %% Task 1 – Power per rotor in hover (Ingenuity, Mars)
 
-% === Constants ===
+%  Constants
 g = 3.73;                      % Mars gravity [m/s^2]
 rho = 20e-3;                    % Mars atmospheric density [kg/m^3]
 C_d0 = 0.02;                    % Profile drag coefficient
@@ -14,19 +14,19 @@ R = 0.6;                        % Rotor radius [m]
 m = 1.8;                        % Total mass [kg]
 gamma = 1.15;                   % Correction factor
 
-% === Area===
+% Area
 A = pi * R^2;                   % Rotor disk area [m^2]
 
-% === Thrust ===
+% Thrust 
 T = (m * g)/2;                  % Thrust per rotor [N]
 
 
-% === Power Calculations ===
+% Power Calculations 
 P_ideal = T^(1.5) / sqrt(2 * rho * A);                      % Ideal hover power [W]
 P_0 = (1/8) * rho * c * Nb * C_d0 * omega^3 * R^4;          % Profile drag power [W]
 P_total = gamma * P_ideal + P_0;                            % Total power per rotor [W]
 
-% === Output ===
+% Output
 fprintf('Ideal hover power per rotor       : %.2f W\n', P_ideal);
 fprintf('Profile drag power per rotor      : %.2f W\n', P_0);
 fprintf('Total hover power per rotor       : %.2f W\n', P_total);
